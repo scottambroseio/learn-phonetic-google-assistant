45997c0a1a2dbdc801560974d8122985706271db
Scott Ranger <millenniumnight@googlemail.com>
2017-05-21 20:12:47 +0100

    Real test for phonetic-for action

--- src/actions/__tests__/phonetic-for.test.js
+++ src/actions/__tests__/phonetic-for.test.js
@@ -3,9 +3,14 @@
 import phoneticFor from '../phonetic-for';
 
 describe('phonetic-for', () => {
-  it('should not error', () => {
-    expect(() =>
-      phoneticFor({ tell: () => {}, getArgument: () => '' })
-    ).not.toThrow();
+  it('should generate a correct response', () => {
+    const appMock = { tell: jest.fn(), getArgument: () => 'b' };
+    const appMock2 = { tell: jest.fn(), getArgument: () => 'C' };
+
+    phoneticFor(appMock);
+    phoneticFor(appMock2);
+
+    expect(appMock.tell.mock.calls[0][0]).toMatchSnapshot();
+    expect(appMock2.tell.mock.calls[0][0]).toMatchSnapshot();
   });
 });
