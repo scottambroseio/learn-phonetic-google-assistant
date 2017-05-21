42c16484c041b6ff3e799486dcc41b26eb3a5124
Scott Ranger <millenniumnight@googlemail.com>
2017-05-21 18:16:04 +0100

    phonetic-for action MVP

--- src/actions/phonetic-for.js
+++ src/actions/phonetic-for.js
@@ -1,8 +1,37 @@
 // @flow
 
+const map = {
+  a: 'alfa',
+  b: 'bravo',
+  c: 'charlie',
+  d: 'delta',
+  e: 'echo',
+  f: 'foxtrot',
+  g: 'golf',
+  h: 'hotel',
+  I: 'india',
+  j: 'juliett',
+  k: 'kilo',
+  l: 'lima',
+  m: 'mike',
+  n: 'november',
+  o: 'oscar',
+  p: 'papa',
+  q: 'quebec',
+  r: 'romeo',
+  s: 'sierra',
+  T: 'tango',
+  u: 'uniform',
+  v: 'victor',
+  w: 'whiskey',
+  x: 'xray',
+  y: 'yankee',
+  z: 'zulu'
+};
+
 export default (app: any) => {
   // arg names end up in lowercase - remember this!
-  const letter = app.getArgument('letter');
+  const letter = app.getArgument('letter').toLowerCase();
 
-  app.ask(`You asked for the phonetic for the letter ${letter}`);
+  app.tell(`The phonetic for the letter ${letter} is ${map[letter]}`);
 };
